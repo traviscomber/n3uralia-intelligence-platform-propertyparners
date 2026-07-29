8642f937e8aa2fd59875a177e56fb3428803f484
traviscomber <n3uralia@gmail.com>
2026-07-29 13:59:01 -0400

    feat(control): add contractual partner dashboard

---
+++ app/dashboard/partner/page.tsx
@@ -0,0 +1,5 @@
+import { ManagementRoleDashboard } from '@/components/management/role-dashboard'
+
+export default function PartnerDashboard() {
+  return <ManagementRoleDashboard view="partner" />
+}
