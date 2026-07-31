8bbd378a707915d2617efc2538235f144e0898a7
traviscomber <n3uralia@gmail.com>
2026-07-30 21:22:26 -0400

    Connect partner dashboard to scoped operational modules

--- app/dashboard/partner/page.tsx
+++ app/dashboard/partner/page.tsx
@@ -1,5 +1,9 @@
 import { ManagementRoleDashboard } from '@/components/management/role-dashboard'
+import { PartnerOperationalWorkspace } from '@/components/management/partner-operational-workspace'
 
 export default function PartnerDashboard() {
-  return <ManagementRoleDashboard view="partner" />
+  return <>
+    <ManagementRoleDashboard view="partner" />
+    <PartnerOperationalWorkspace />
+  </>
 }
