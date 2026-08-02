eb0f329e8e6f624d56d7651937150aac141e3500
traviscomber <n3uralia@gmail.com>
2026-08-02 17:03:22 -0400

    feat: render N3uralia intelligence in CEO decisions

--- app/dashboard/ceo/decisiones/page.tsx
+++ app/dashboard/ceo/decisiones/page.tsx
@@ -1,5 +1,9 @@
 import { CeoDecisions } from '@/components/management/ceo-decisions'
+import { CeoIntelligencePanel } from '@/components/management/ceo-intelligence-panel'
 
 export default function CeoDecisionsPage(){
-  return <CeoDecisions />
+  return <div className="space-y-6">
+    <CeoIntelligencePanel />
+    <CeoDecisions />
+  </div>
 }
