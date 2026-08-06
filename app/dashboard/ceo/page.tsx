2e8750c4d5a0a8c959d1b61d9702337f38239d6a
traviscomber <n3uralia@gmail.com>
2026-08-06 00:33:56 -0400

    fix: simplify CEO dashboard to decision data

--- app/dashboard/ceo/page.tsx
+++ app/dashboard/ceo/page.tsx
@@ -1,11 +1,5 @@
 import { CeoDashboardV2 } from '@/components/management/ceo-dashboard-v2'
-import { CeoIntelligencePanel } from '@/components/management/ceo-intelligence-panel'
-import { DataLayerLegend } from '@/components/management/data-layer-legend'
 
 export default function CeoDashboard() {
-  return <>
-    <DataLayerLegend showProvisionalRules />
-    <CeoIntelligencePanel />
-    <CeoDashboardV2 />
-  </>
+  return <CeoDashboardV2 />
 }
