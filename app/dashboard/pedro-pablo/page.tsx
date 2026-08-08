74151a55d5024bcb482a6f99415726b68dc3665d
traviscomber <n3uralia@gmail.com>
2026-08-07 23:42:13 -0400

    refactor: unify Pedro Pablo property context

--- app/dashboard/pedro-pablo/page.tsx
+++ app/dashboard/pedro-pablo/page.tsx
@@ -1,9 +1,5 @@
-import { PedroPabloPropertyContext } from '@/components/intelligence/pedro-pablo-property-context'
 import { PedroPabloWorkspace } from '@/components/intelligence/pedro-pablo-workspace'
 
 export default function PedroPabloPage() {
-  return <div className="space-y-8">
-    <PedroPabloWorkspace />
-    <PedroPabloPropertyContext />
-  </div>
+  return <PedroPabloWorkspace />
 }
