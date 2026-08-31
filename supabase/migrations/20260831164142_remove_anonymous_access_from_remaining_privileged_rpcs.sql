ee6577858ec4a63721e5ae73f8086c2db739e16e
traviscomber <n3uralia@gmail.com>
2026-08-31 12:47:49 -0400

    chore: align anonymous RPC lockdown migration version

---
+++ supabase/migrations/20260831164142_remove_anonymous_access_from_remaining_privileged_rpcs.sql
@@ -0,0 +1,6 @@
+revoke execute on function public.lookup_vitacura_prc_v1(double precision,double precision) from public, anon;
+revoke execute on function public.valuation_house_lo_curro_advisory_v1(uuid) from public, anon;
+revoke execute on function public.valuation_house_lo_curro_advisory_v2(uuid) from public, anon;
+revoke execute on function public.valuation_house_regime_predict_case_v1(uuid) from public, anon;
+revoke execute on function public.valuation_house_regime_shadow_case_v1(uuid) from public, anon;
+revoke execute on function public.valuation_professional_review_v1(uuid) from public, anon;
