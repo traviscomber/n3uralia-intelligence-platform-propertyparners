5be7d6ffde8caadfbaa2471e1f289de25d415dbb
traviscomber <n3uralia@gmail.com>
2026-08-31 12:48:18 -0400

    chore: remove mismatched migration version

--- supabase/migrations/20260831163925_harden_management_views_and_regime_router_rules.sql
+++
@@ -1,6 +0,0 @@
-alter view public.management_monthly_source_aggregates set (security_invoker = true);
-alter view public.management_verified_monthly_source_aggregates set (security_invoker = true);
-
-alter table public.valuation_house_regime_router_rules enable row level security;
-revoke all on table public.valuation_house_regime_router_rules from anon, authenticated;
-grant select on table public.valuation_house_regime_router_rules to service_role;
