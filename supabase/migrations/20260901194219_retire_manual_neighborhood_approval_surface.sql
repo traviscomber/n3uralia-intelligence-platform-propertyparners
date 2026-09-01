74822a6998b12bf8133ce8fcd693051fc19bb170
traviscomber <n3uralia@gmail.com>
2026-09-01 15:42:36 -0400

    security: retire manual neighborhood approval surface

---
+++ supabase/migrations/20260901194219_retire_manual_neighborhood_approval_surface.sql
@@ -0,0 +1,4 @@
+drop function if exists public.approve_ceo_market_neighborhood_batch_v1(text);
+drop function if exists public.correct_ceo_market_neighborhood_conflicts_v1();
+drop function if exists public.get_ceo_market_neighborhood_batch_summary_v1();
+drop function if exists public.get_ceo_market_neighborhood_conflicts_v1();
